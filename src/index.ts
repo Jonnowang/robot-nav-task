00823d94340f250a4074a1be798b54b6dd9d48a1
Jonathan Wang <jw2055@cam.ac.uk>
2022-03-17 16:04:34 +0000

    fix row and column

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import Game from './game'
 
 console.log("Welcome to the toy robot simulator!")
 
+// Print command information
 function helpInfo() {
     console.log("\nValid commands are:\n\nPLACE_ROBOT - Places a robot if none exist\nPLACE_WALL - Places an impassable wall\nREPORT - Print current location and facing of robot\nMOVE - Move robot forward one space\nLEFT/RIGHT - Turn the robot 90 degrees left or right\nHELP - Print these instructions again\nEXIT - Quit the program")
 }
@@ -13,18 +14,49 @@ function checkInvalidDir(s: string) {
     return true
 }
 
+// Function to place robot and check inputs are valid
+function placeRobot(g: Game) {
+    let success = false
+    let row: number = input("(Enter between 1-5) Row: ")
+    if (isNaN(row)) placeRobot(g)
+    let col: number = input("(Enter between 1-5) Column: ")
+    if (isNaN(col)) placeRobot(g)
+    let facing: Direction = input("(NORTH, EAST, SOUTH or WEST) Facing: ")
+    if (checkInvalidDir(facing)) placeRobot(g)
+    success = g.PLACE_ROBOT(row,col,facing)
+    if (!success) {
+        console.log("Please use valid inputs!")
+        placeRobot(g)
+    }
+}
+
+// Function to place a wall
+function placeWall(g: Game) {
+    let success_wall = false
+    let row: number = input("(Enter between 1-5) Row: ")
+    if (isNaN(row)) placeWall(g)
+    let col: number = input("(Enter between 1-5) Column: ")
+    if (isNaN(col)) placeWall(g)
+    success_wall = g.PLACE_WALL(row,col)
+    if (!success_wall) {
+        console.log("Please use valid inputs!")
+        placeWall(g)
+    }
+}
+
 helpInfo()
 
 const input: NodeRequire = require('prompt-sync')();
 let command = ""
 let g = new Game()
 
-while (command != "EXIT") {
+// Execute commands based on user input
+setInterval(() => {
     command = input("Robot Input> ");
     switch(command) {
         case 'EXIT':
             console.log('Hope you enjoyed the game!');
-            break;
+            process.exit();
         case 'HELP':
             helpInfo();
             break;
@@ -41,35 +73,13 @@ while (command != "EXIT") {
             g.REPORT();
             break;
         case 'PLACE_ROBOT':
-            let success = false
-            while (!success) {
-                let row: number = input("(Enter between 1-5) Row: ")
-                if (isNaN(row)) continue
-                let col: number = input("(Enter between 1-5) Column: ")
-                if (isNaN(col)) continue
-                let facing: Direction = input("(NORTH, EAST, SOUTH or WEST) Facing: ")
-                if (checkInvalidDir(facing)) continue
-                success = g.PLACE_ROBOT(col,row,facing)
-                if (!success) {
-                    console.log("Please use valid inputs!")
-                }
-            }
+            placeRobot(g);
             break;
         case 'PLACE_WALL':
-            let success_wall = false
-            while (!success_wall) {
-                let row: number = input("(Enter between 1-5) Row: ")
-                if (isNaN(row)) continue
-                let col: number = input("(Enter between 1-5) Column: ")
-                if (isNaN(col)) continue
-                success_wall = g.PLACE_WALL(col,row)
-                if (!success_wall) {
-                    console.log("Please use valid inputs!")
-                }
-            }
+            placeWall(g);
             break;
         default:
             console.log("Please enter a valid command.");
         break;
     }
-}
+}, 0)
